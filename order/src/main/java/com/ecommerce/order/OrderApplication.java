e3f95c8b47ff34e28ef67105fd87a47833a2dba5
Ningappa-Tambad <ningaraj2t@gmail.com>
2025-06-19 19:55:07 +0530

    Implemented user,Product,cart and order separate services

--- order/src/main/java/com/ecommerce/order/OrderApplication.java
+++ order/src/main/java/com/ecommerce/order/OrderApplication.java
@@ -2,8 +2,11 @@ package com.ecommerce.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.ecommerce.order.repositories")
+
 public class OrderApplication {
 
 	public static void main(String[] args) {
